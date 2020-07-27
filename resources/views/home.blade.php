07106087b01744fe45d05f0875ec0835acac43bc
monica722 <38679283+monica722@users.noreply.github.com>
2020-07-27 17:54:01 +0800

    baru
    baru

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
     <BR>
     <BR>
-    <BR>
+    <BR>  
     <BR>  
     <h2 align="center">SELAMAT DATANG DI</h2>
     <h2 align="center">SISTEM INFORMASI PENILAIAN SISWA</h2>
